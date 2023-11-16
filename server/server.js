ea2e98c5532f10fa78e52ab19835fd6a0ceba4ce
Antonios Alexopoulos <xviia42@gmail.com>
2023-11-16 15:57:08 +0200

    add active users bar

--- server/server.js
+++ server/server.js
@@ -10,24 +10,40 @@ const io = new Server(httpServer, {
 	}
 });
 
-let totalClients = 0
+let totalClients = 0;
+
+const activeUsers = new Map();
 
 io.on("connection", (socket) => {
 	totalClients++;
 	console.log(`[+] client connected : ${socket.id}`);
 	socket.broadcast.emit("client_status", {clients_available : totalClients});
 
+	socket.broadcast.emit("active_users", serializeMapUsers(activeUsers));
+	socket.emit("active_users", serializeMapUsers(activeUsers));
+
 	socket.on("disconnect", () => {
 		console.log(`[+] client disconnected : ${socket.id}`);
 		totalClients--;
+		
+		activeUsers.delete(socket.id);
+		socket.broadcast.emit("active_users", serializeMapUsers(activeUsers));
+
 		socket.broadcast.emit("client_status", {clients_available : totalClients});
 	});
 
 	socket.on("message", (msg) => {
-
-		// console.log(msg + socket.id);
 		socket.broadcast.emit("message", msg);
 	});
+
+	socket.on("add_user", (userObj) => {
+		activeUsers.set(socket.id,userObj.name);
+
+		const activeUsersObj = serializeMapUsers(activeUsers);
+
+		socket.emit("active_users", activeUsersObj);
+		socket.broadcast.emit("active_users", activeUsersObj);
+	})
 
 	socket.emit("server_response", new serverStats("primary",socket.id,totalClients));
 
@@ -41,4 +57,17 @@ function serverStats(serverName,toClient,clientsCounter) {
 	this.serverName = serverName;
 	this.toClient = toClient;
 	this.totalClients = clientsCounter;
+}
+
+function serializeMapUsers(activeUsersState) {
+	const mapIter = activeUsersState.values();
+
+	let responseUsersObj = new Array();
+
+	for (let _i=0; _i < activeUsersState.size; _i++) {
+		let name = mapIter.next().value;
+		responseUsersObj.push(name);
+	}
+
+	return {active_users : responseUsersObj};
 }
--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { io } from "socket.io-client";
 
-
 function App() {
   
   const [client,setClient] = useState(null);
@@ -11,6 +10,7 @@ function App() {
   const [serverStats,setServerStats] = useState(null);
   const [clientName,setClientName] = useState(null);
   const [usernameState,setUsernameState] = useState(true);
+  const [activeUsers,setActiveUsers] = useState([]);
   const txtArea = useRef(null);
 
   useEffect(() => {
@@ -20,7 +20,14 @@ function App() {
     });
 
     socket.on("connect_error", () => {
+      setClientName(null);
       setConnected(false);
+      setInbox([]);
+      setMsg("");
+      setClient(null);
+      setServerStats(null);
+      setUsernameState(true);
+      setActiveUsers([]);
     });
 
     setClient(socket);
@@ -39,6 +46,10 @@ function App() {
     socket.on("client_status", (obj) => {
       setServerStats({...serverStats, "totalClients" : obj.clients_available})
     });
+
+    socket.on("active_users", (obj) => {
+      setActiveUsers(obj.active_users);
+    })
 
     return () => {
       socket.disconnect();
@@ -73,28 +84,57 @@ function App() {
     }
   }
 
+  function handleClientName(e) {
+    if (e.target.value <= 0) {
+      return;
+    }
+
+    const candidateName = e.target.value;
+
+    setClientName(candidateName.toUpperCase().slice(0,1) + candidateName.toLowerCase().slice(1));
+  }
+
+  function lockUserName() {
+    if (clientName === null) {
+      return;
+    }
+
+    const userExists = activeUsers.find(activeName => activeName === clientName);
+
+    if (userExists !== undefined) {
+      alert("User name not available at the moment.");
+      return;
+    }
+
+    if (client && clientName.lenght !== 0) {
+      client.emit("add_user", {name : clientName});
+    }
+    setUsernameState(false);
+  }
+
   return (
     <>
       <h2>Talk</h2>
       <div id="container">
-        <ul>
+        <ul className="chat">
+          <li>Live Message Feed</li>
           {
             inbox.lenght !== 0 &&
             inbox.map((item,index) => {
               if (item.from === clientName) {
                 return <li 
                   key={index} 
-                >Me[{item.timestamp}] : {item.message}</li>
+                >Me [{item.timestamp}]: {item.message}</li>
               } else {
                 return <li 
                   key={index}
-                >{item.from}[{item.timestamp}] : {item.message}</li>
+                >{item.from} [{item.timestamp}]: {item.message}</li>
               }
             })
           }
         </ul>
         <div id="box">
-          <div class="status-head">
+          <div className="status-head">
             {connected ? 
               <h4>Status: Connected</h4> 
               : 
@@ -107,12 +147,27 @@ function App() {
           {(usernameState) ?
             <div id="client-name">
               <input placeholder='Type your username...' 
-                    onInput={(e) => setClientName(e.target.value)}/>
-              <button onClick={() => setUsernameState(false)}>Submit</button>
+                    onInput={handleClientName}/>
+              <button onClick={lockUserName}>Submit</button>
             </div>
             : <h4>name: {clientName}</h4>
-          } 
+          }
+          <ul className="active-users">
+            <li style={{color : "yellow"}}>Active Users</li>
+            {
+              activeUsers.length !== 0 &&
+              activeUsers.map((item,index) => {
+
+                if (item === clientName) {
+                  return <li key={index}>{item}[Me]</li>
+                } else {
+                  return <li key={index}>{item}</li>
+                }
+              })
+            }
+          </ul>
         </div>
+
       </div>
     </>
   )
